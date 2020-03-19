333d60ae1a53622e2fc822ca8160466761050d1a
Hayakawa_ii <iamclusterfuzz@gmail.com>
2020-03-19 16:10:24 +0800

    Updates

---
+++ precache-manifest.4e13d19dd8710c14d3d580c97a68aba3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6530cbd741264af3e01c62c548af534f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f3bff5e4d14d3fdfb6aa",
+    "url": "/monsters-rolodex/static/css/main.e5c79eda.chunk.css"
+  },
+  {
+    "revision": "4ecb9f0df0474c1b1fa9",
+    "url": "/monsters-rolodex/static/js/2.47062208.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.47062208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3bff5e4d14d3fdfb6aa",
+    "url": "/monsters-rolodex/static/js/main.9c882bab.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
